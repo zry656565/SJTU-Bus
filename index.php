bb1451acd2b8a83591b6edcb7ff49af342a80537
邹润阳 <zry656565@gmail.com>
2014-11-10 21:33:42 +0800

    php版本初始化

--- index.php
+++ index.php
@@ -1,14 +1,29 @@
-<!DOCTYPE html>  
+<?php
+// Include and instantiate the class.
+require_once 'include/Mobile_Detect.php';
+$detect = new Mobile_Detect;
+$version = "web";
+
+if( $detect->isMobile() && !$detect->isTablet() ){
+    $version = "wap";
+}
+?>
+
+<!DOCTYPE html>
 <html>  
     <head>  
         <meta name="viewport" content="initial-scale=1.0, user-scalable=no" />  
         <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />  
         <title>上海交通大学校园巴士时刻表 by JerryZou</title>
-        <link href="css/main.wap.css" rel="stylesheet"/>
+        <link href="css/main.<?=$version?>.css" rel="stylesheet"/>
         <script type="text/javascript" src="http://api.map.baidu.com/api?v=2.0&ak=kSmEaa2spbYKGxtao1FdpVGq"></script>
+        <?php if ($version === "web") { ?>
+        <script type="text/javascript" src="js/jquery-1.11.1.min.js"></script>
+        <?php } else { ?>
         <script type="text/javascript" src="js/zepto.min.js"></script>
+        <?php } ?>
         <script type="text/javascript" src="js/utility.js"></script>
-        <script type="text/javascript" src="js/main.wap.js"></script>
+        <script type="text/javascript" src="js/main.<?=$version?>.js"></script>
         <!-- baidu site center -->
         <script>
             var _hmt = _hmt || [];
@@ -24,6 +39,11 @@
  
     <body>
         <div id="container"></div>
+
+        <?php if ($version === "web") { ?>
+        <!-- fork me on github -->
+        <a id="forkme" href="https://github.com/zry656565/SJTU-Bus"><img style="position: absolute; top: 0; right: 0; border: 0;" src="images/fork.png" alt="Fork me on GitHub"></a>
+        <?php } ?>
 
         <!-- feedback -->
         <a href="http://jerryzou.com/posts/sjtuBusFeedback/"><div id='feedback'>意见反馈</div></a>
