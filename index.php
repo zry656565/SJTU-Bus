cafd62672295ff9b91a3f61b1bfa81b0488fad16
Jerry Zou <zry656565@gmail.com>
2015-02-10 08:25:16 +0800

    重构引入资源文件的方式

--- index.php
+++ index.php
@@ -11,26 +11,8 @@ $version = ($detect->isMobile() && !$detect->isTablet()) ? "wap" : "web";
         <meta name="viewport" content="initial-scale=1.0, user-scalable=no" />  
         <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />  
         <title>上海交通大学校园巴士时刻表 by JerryZou</title>
-        <link href="css/main.<?=$version?>.css?v=1.0.2" rel="stylesheet"/>
-        <script type="text/javascript" src="http://api.map.baidu.com/api?v=2.0&ak=kSmEaa2spbYKGxtao1FdpVGq"></script>
-        <?php if ($version === "web") { ?>
-        <script type="text/javascript" src="js/jquery-1.11.1.min.js"></script>
-        <?php } else { ?>
-        <script type="text/javascript" src="js/zepto.min.js"></script>
-        <?php } ?>
-        <script type="text/javascript" src="js/utility.js?v=1.0.1"></script>
-        <script type="text/javascript" src="js/main.<?=$version?>.js?v=1.0.1"></script>
-        <!-- baidu site center -->
-        <script>
-            var _hmt = _hmt || [];
-            (function() {
-                var hm = document.createElement("script");
-                hm.src = "//hm.baidu.com/hm.js?f4dc438d8e60d1de1b6d1a9f197ddfdc";
-                var s = document.getElementsByTagName("script")[0]; 
-                s.parentNode.insertBefore(hm, s);
-            })();
-        </script>
-        <!-- baidu end -->
+        <?php if ($version == 'wap')  {?> <link href="css/main.wap.css" rel="stylesheet"/> <?php } ?>
+        <?php if ($version == 'web')  {?> <link href="css/main.web.css" rel="stylesheet"/> <?php } ?>
     </head>
  
     <body>
@@ -42,6 +24,26 @@ $version = ($detect->isMobile() && !$detect->isTablet()) ? "wap" : "web";
         <?php } ?>
 
         <!-- feedback -->
-        <a href="http://jerryzou.com/posts/sjtuBusFeedback/"><div id='feedback'>意见反馈</div></a>
-    </body>  
+        <a href="http://jerryzou.com/posts/sjtuBusFeedback/">
+            <div id='feedback'>意见反馈</div>
+        </a>
+
+        <!-- script -->
+        <script src="//api.map.baidu.com/api?v=2.0&ak=kSmEaa2spbYKGxtao1FdpVGq"></script>
+        <?php if ($version === "wap") { ?> <script src="js/zepto.min.js"></script> <?php } ?>
+        <?php if ($version === "web") { ?> <script src="js/jquery-1.11.1.min.js"></script> <?php } ?>
+        <script src="js/utility.js"></script>
+        <?php if ($version === "wap") { ?> <script src="js/main.wap.js"></script> <?php } ?>
+        <?php if ($version === "web") { ?> <script src="js/main.web.js"></script> <?php } ?>
+        <!-- baidu site center -->
+        <script>
+            var _hmt = _hmt || [];
+            (function() {
+                var hm = document.createElement("script");
+                hm.src = "//hm.baidu.com/hm.js?f4dc438d8e60d1de1b6d1a9f197ddfdc";
+                var s = document.getElementsByTagName("script")[0];
+                s.parentNode.insertBefore(hm, s);
+            })();
+        </script>
+    </body>
 </html>
